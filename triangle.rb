0f3b76e96516eafbb2276cee350d14bac7808b05
Justin NORR <jnorthageorr@gmail.com>
2016-03-27 11:48:31 -0400

    Tests written: 165/282

--- triangle.rb
+++ triangle.rb
@@ -14,14 +14,19 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  if a == b && a == c && b == c
-  	:equilateral
+  if a <= 0 || b <= 0 || c <= 0
+  		raise TriangleError, "Sides must be greater than zero"
+  elsif a + b <= c || a + c <= b || b + c <= a
+  		raise TriangleError, "sum of any two sides must be longer than the third side"
+  elsif a == b && a == c && b == c
+  		:equilateral
   elsif a != b && a != c && b != c
-  	:scalene 
-  else 
-  	:isosceles
+  		:scalene 
+  else
+  		:isosceles
   end
 end
+
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
